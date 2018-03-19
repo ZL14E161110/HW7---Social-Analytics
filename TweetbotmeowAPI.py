a5b390828b7c57a24fa4236375eb2f7b481e3364
ZL14E161110 <34262977+ZL14E161110@users.noreply.github.com>
2018-03-19 00:35:31 -0700

    Upload API Key

---
+++ TweetbotmeowAPI.py
@@ -0,0 +1,10 @@
+api_key = "W4H88764hQtQ6WFYzALy98fkx"
+
+api_secret = "ROTKyWNTpcZFGDArUw71q7G6Ol8pRgRBKthXjvPcrCtYGgpwhN"
+
+access_token ="975632361227476992-nqt4FIZln9Ro5iP1lEnubuKsnbdyD5I"
+
+access_token_secret = "3PtxXcqyqLjVbXoIccA1JFREdsUE1ELQkjPvDIQKM6PXN"
+
+
+
